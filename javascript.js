5a025e5a9818b5a3543c97671f2aae294ad26a77
IIHEXYII <vetletessem@gmail.com>
2022-09-22 13:56:18 +0200

    First commit portfolio website

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  navigator.clipboard.writeText(copyText.value);
+
+  alert("Copied " + copyText.value + "to clipboard!");
+}
+
+var newsrc = "velte.jpg";
+
+function changeImage() {
+  if (newsrc == "velte.jpg") {
+    document.images["avatar"].src = "/assets/hexy.gif";
+    document.images["avatar"].alt = "meme";
+    newsrc = "Hexy.gif";
+  } else {
+    document.images["avatar"].src = "/assets/velte.jpg";
+    document.images["avatar"].alt = "me";
+    newsrc = "velte.jpg";
+  }
+}
